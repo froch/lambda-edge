b1206b6fafd2cadd3b1aa06a42a224117cbf49cd
Francis Roch <francis@manafont.net>
2024-10-03 08:39:24 -0400

    pnpm lint / pnpm fmt

---
+++ lambda/eslint.config.js
@@ -0,0 +1,24 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': 'off',
+    },
+  },
+];
