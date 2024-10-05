7c4c82e466100359333bd4386ad570d7c6494ccc
Francis Roch <francis@manafont.net>
2024-10-04 22:28:22 -0400

    Add log_error

--- tools/scripts/logs.sh
+++ tools/scripts/logs.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 export NC='\033[0m'
 export BL_GREEN='\033[0;92m'
 export BL_GRAY='\033[0;90m'
+export BL_RED='\033[0;91m'
 export B_GRAY='\033[1;37m'
 
 # log functions
@@ -12,9 +13,13 @@ log(){
 }
 
 log_info(){
-  # $1 is the module name
-  # $2 is the message
-  set +u
-  log "${BL_GREEN}INFO${NC} ${BL_GRAY}$1${NC} // ${B_GRAY}$2${NC}"
-  set -u
+  local mod=$1
+  local msg=$2
+  log "${BL_GREEN}INFO${NC} ${BL_GRAY}$mod${NC} // ${B_GRAY}$msg${NC}"
+}
+
+log_error(){
+  local mod=$1
+  local msg=$2
+  log "${BL_RED}ERROR${NC} ${BL_GRAY}$mod${NC} // ${B_GRAY}$msg${NC}"
 }
