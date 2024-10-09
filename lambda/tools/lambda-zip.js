e5dc7096c7c96045da153c06d649b979a5fb72ae
Francis Roch <francis@manafont.net>
2024-10-09 17:49:52 -0400

    Upload lambda zip to S3

--- lambda/tools/lambda-zip.js
+++ lambda/tools/lambda-zip.js
@@ -9,25 +9,72 @@ const zipDir = path.join(projectRoot, 'zip');
 const zipFileName = `lambda-${timestamp}.zip`;
 const zipFilePath = path.join(zipDir, zipFileName);
 
-try {
-  if (!fs.existsSync(zipDir)) {
-    fs.mkdirSync(zipDir);
+// -------- internals -------- //
+
+const _getenv = (name) => {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Environment variable ${name} is required`);
   }
-  process.chdir(pkgDir);
+  return value;
+};
+
+const _mkdir = (dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+};
+
+const _mkZip = (sourceDir, zipFilePath) => {
+  process.chdir(sourceDir);
   execSync(`zip -r ${zipFilePath} .`, { stdio: 'inherit' });
   process.chdir(projectRoot);
   console.log(`created ${zipFilePath}`);
+};
 
+const _zipExists = (zipFilePath, maxSizeMB) => {
   const stats = fs.statSync(zipFilePath);
-  const fileSizeInBytes = stats.size;
-  const fileSizeInMB = fileSizeInBytes / (1024 * 1024);
-
-  if (fileSizeInMB > 1) {
-    throw new Error(
-      `Zip size is ${fileSizeInMB.toFixed(2)} MB. Lambda@Edge "viewerRequest" limit is 1MB.`
-    );
+  const fileSizeInMB = stats.size / (1024 * 1024);
+  if (fileSizeInMB > maxSizeMB) {
+    throw new Error(`Zip size is ${fileSizeInMB.toFixed(2)} MB. Lambda@Edge "viewerRequest" limit is ${maxSizeMB}MB.`);
   }
-} catch (error) {
-  console.error('Zip creation failed:', error);
-  process.exit(1);
+};
+
+const _cp = (source, destination) => {
+  fs.copyFileSync(source, destination);
+  console.log(`created ${destination}`);
+};
+
+const _s3upload = (filePath, s3dirname) => {
+  const fileName = path.basename(filePath);
+  const fullUploadPath = `${s3dirname}/${fileName}`;
+  console.log(`aws s3 cp ${filePath} ${fullUploadPath}`);
+  execSync(`aws s3 cp ${filePath} ${fullUploadPath}`, { stdio: 'inherit' });
+};
+
+// -------- main -------- //
+
+const main = () => {
+  try {
+    const s3bucket = _getenv('AWS_S3_BUCKET_NAME');
+    const s3path = _getenv('AWS_S3_BUCKET_PATH');
+    const s3dirname = `s3://${s3bucket}/${s3path}`;
+
+    _mkdir(zipDir);
+    _mkZip(pkgDir, zipFilePath);
+    _zipExists(zipFilePath, 1);
+
+    const latestZipFilePath = path.join(zipDir, 'lambda-latest.zip');
+    _cp(zipFilePath, latestZipFilePath);
+
+    _s3upload(zipFilePath, s3dirname);
+    _s3upload(latestZipFilePath, s3dirname);
+  } catch (error) {
+    console.error('Zip job failed:', error);
+    process.exit(1);
+  }
 }
+
+// -------- entrypoint -------- //
+
+main();
